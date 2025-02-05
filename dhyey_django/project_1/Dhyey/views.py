3829fb7860e0b3ec873338b241a8810d8d9c25f8
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-05 14:03:52 +0530

    Update views.py

--- dhyey_django/project_1/Dhyey/views.py
+++ dhyey_django/project_1/Dhyey/views.py
@@ -6,3 +6,14 @@ from rest_framework.response import Response
 @api_view(["GET"])
 def company(request):
     return Response({'status':200,'message':'right'})
+
+
+
+@api_view(['POST'])
+def demos(request):
+    data = request.data
+    serializer = Studentserializers(data = request.data)
+    if not serializer.is_valid():
+        return Response({'status' : 201,'massage' : 'wrong'})
+    serializer.save()
+    return Response({'STATUS' : 200,'MASSAGE' : 'RIGHT', 'PAYLOAD' : serializer.data})
