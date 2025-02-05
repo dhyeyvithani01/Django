4e911e6cfa72879719032dcb8ef8717241c5bc98
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-05 14:05:29 +0530

    Update models.py

--- dhyey_django/project_1/Dhyey/models.py
+++ dhyey_django/project_1/Dhyey/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class Employers(models.Model):
+class Student(models.Model):
     emplyees = models.CharField(max_length=100)
     age = models.IntegerField(default=30)
