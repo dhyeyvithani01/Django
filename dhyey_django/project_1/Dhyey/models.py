6a07a26e43c49d8d70c939b208ffbc44c738b08f
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-07 14:47:13 +0530

    Update models.py

--- dhyey_django/project_1/Dhyey/models.py
+++ dhyey_django/project_1/Dhyey/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Student(models.Model):
     emplyees = models.CharField(max_length=100)
     age = models.IntegerField(default=30)
-    email_id = models.CharField(max_length=30)
+    email_id = models.CharField(max_length=30,default="")
