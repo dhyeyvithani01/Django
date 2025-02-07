df61154943e09d8c482b21c9b55b10d34ecef3d0
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-07 15:06:45 +0530

    Update serializers.py

--- dhyey_django/project_1/Dhyey/serializers.py
+++ dhyey_django/project_1/Dhyey/serializers.py
@@ -5,3 +5,8 @@ class Studentserializers(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields ="__all__"
+        
+class Teacherserializers(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields ="__all__"
