3a6ad65b0c2c91fc4a53332d1c697c495ae1452f
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-18 15:38:04 +0530

    1

--- dhyey_django/project_1/Dhyey/models.py
+++ dhyey_django/project_1/Dhyey/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField(default=30)
-    email_id = models.EmailField(max_length=30)
-    mobile_no = models.CharField(max_length=10)
-    address = models.TextField(max_length=100)
+    Name = models.CharField(max_length=100)
+    Age = models.IntegerField(default=30)
+    Email_id = models.EmailField(max_length=30)
+    Mobile_no = models.CharField(max_length=10)
+    Address = models.TextField(max_length=100)
 
 
 # class Teacher(models.Model):
--- dhyey_django/project_1/Dhyey/serializers.py
+++ dhyey_django/project_1/Dhyey/serializers.py
@@ -6,6 +6,32 @@ class Studentserializers(serializers.ModelSerializer):
         model = Student
         fields ="__all__"
         
+    def validate(self,data):
+        
+        # Name Validtion
+        if not data["Name"].isalpha():
+            raise serializers.ValidationError("Name must only contain alphabates")
+        
+        # Age validation
+        if data["Age"]<18:
+            raise serializers.ValidationError("The person must be at least 18 years old")
+        
+        #Email validation
+        email=data.get("Email_id")
+        if data ["Email_id"] == '@gmail.com':
+            raise serializers.ValidationError("Email Is Not Valid")
+        
+        elif email[0].isdigit():
+            raise serializers.ValidationError("Email Is Not Valid")
+        
+        elif not email.islower():
+            raise serializers.ValidationError("Email Is Not Valid")
+              
+        elif not email.endswith('@gmail.com'):
+            raise serializers.ValidationError("Email Is Not Valid")
+        
+        return data
+        
 # class Teacherserializers(serializers.ModelSerializer):
 #     class Meta:
 #         model = Teacher
