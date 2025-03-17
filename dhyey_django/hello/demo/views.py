5feb44d30c11b8af6c37abd86102100316cf5546
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-17 13:23:59 +0530

    1

--- dhyey_django/hello/demo/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-
-@api_view(['GET'])
-def dhyey(request):
-    return Response({'status' : 200,'message' : 'right' })
