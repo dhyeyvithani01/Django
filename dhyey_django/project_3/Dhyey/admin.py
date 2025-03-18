2f7cd859f1d7caec9d045129118f7e57c1995929
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-18 15:42:37 +0530

    1

--- dhyey_django/project_3/Dhyey/admin.py
+++ dhyey_django/project_3/Dhyey/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-@admin.register(student)
+@admin.register(Student)
 class studentAdmin(admin.ModelAdmin):
     list_display=['id','name','age','address','email']
