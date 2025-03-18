64899374e93313be7f0b2edc5191a598f0febbe1
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-18 15:02:58 +0530

    1

---
+++ dhyey_django/project_3/Dhyey/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+
+from.views import *
+
+urlpatterns = [
+    path('',School),
+    path('post/',post),
+    path('put/<id>/',putt),
+    path('patch/<id>/',patchh),
+    path('delete/<id>/',deletee)
+]
