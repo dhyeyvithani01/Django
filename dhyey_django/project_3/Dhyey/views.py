35de32753c8740272f0e2cacfe2778fcaf0a16eb
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-18 14:51:39 +0530

    Update views.py

--- dhyey_django/project_3/Dhyey/views.py
+++ dhyey_django/project_3/Dhyey/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+import json
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse,JsonResponse
 
-# Create your views here.
+@api_view(['Get'])
+def School(request):
+    obj=Student.objects.all()
+    serializer=Studentserializers(obj,many=True)
+    return Response({'status':200,'message':'right','payload':serializer.data})
+
+
+
+@api_view(['POST'])
+def post(request):
+    data = request.data
+    serializer = Studentserializers(data = request.data)
+    if not serializer.is_valid():
+        return Response({'status' : 201,'massage' : 'wrong'})
+    serializer.save()
+    return Response({'STATUS' : 200,'MASSAGE' : 'RIGHT', 'PAYLOAD' : serializer.data})
+
+
+
+
+@api_view(['PUT'])
+def putt (requst,id):
+    try:
+        obj=Student.objects.get(id=id)
+        serializer=Studentserializers(obj,data=requst.data)
+        if not serializer.is_valid():
+            return Response({'status': 201, 'message':'WRONG'})
+        serializer.save()
+        return Response ({'status':200, 'massage':'RIGHT','payload': serializer.data})
+    except Exception as a :
+        return Response({'status':403,'massage':'ERROR'})
+
+    
+@api_view(['PATCH'])
+def patchh (requst,id):
+    try:
+        obj=Student.objects.get(id=id)
+        serializer=Studentserializers(obj,data=requst.data,partial = True)
+        if not serializer.is_valid():
+            return Response({'status': 201, 'message':'WRONG'})
+        serializer.save()
+        return Response ({'status':200, 'massage':'RIGHT','payload': serializer.data})
+    except Exception as d :
+        return Response({'status':403,'massage':'ERROR'})
+    
+    
+@api_view(['DELETE'])
+def deletee(request,id):
+    try:
+        obj=Student.objects.get(id=id)
+        obj.delete()
+        return Response({'status': 200, 'message':"Right"})
+    except Exception as A :
+        return Response({'status':403,'message':'Wrong'})
+
+
+
