a1076f4bb1a2bc38f266d269ee33431a420ae3d3
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-04-14 17:00:12 +0530

    1

--- dhyey_django/project_3/project_3/settings.py
+++ dhyey_django/project_3/project_3/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'project_3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database1',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD' : 'Dhyey_@07',
     }
 }
 
